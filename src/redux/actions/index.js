f44d1871c66f1b04503830ec6fdb37864c55352b
João Pedro <joaopetrussousa@gmail.com>
2022-07-28 15:00:47 -0300

    iniciando o projeto :redux:

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+const actionGenerica = (state) => ({ type: 'ACTION-GENERICA', state });
+
+export const sengundaActionGenerica = (state) => ({ type: 'SEGUNDA-GENERICA', state });
+
+export default actionGenerica;
