3472bb4749d18bcb06f0bd23dbc03e2780ef691a
João Pedro <joaopetrussousa@gmail.com>
2022-07-29 14:47:14 -0300

    Requisito 2 - Completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import newEmail from '../redux/actions';
 
 class Login extends React.Component {
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -9,7 +9,6 @@ const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case 'USER_EMAIL':
     return {
-      ...state,
       email: action.state,
     };
   default:
