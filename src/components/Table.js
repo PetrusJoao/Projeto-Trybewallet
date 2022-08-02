2dc644152e80efb22b42848745c26a0edaab0b2f
João Pedro <joaopetrussousa@gmail.com>
2022-08-02 19:52:33 -0300

    Requisito 6 - Completo

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </table>
+      </div>
     );
   }
 }
+// Tag, Método de pagamento,Valor, Moeda, Câmbio utilizado, Valor convertido, Moeda de conversão e Editar/Excluir.
+export default Table;
